0638038738feee922eaa5faa8d0334a1306b3492
IlyaMBondarev <60775995+IlyaMBondarev@users.noreply.github.com>
2022-01-12 21:15:24 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const colors = require('colors')
+
+function prime(num = 1) {
+    if (typeof num !== 'number') {
+        console.log(`Не число\n`)
+        return
+    }
+
+    let res = 0
+    const colorsForPrimes = ['red', 'yellow', 'green']
+    let indexOfColor = 0
+    const primeNumbers = []
+
+    for(let candidate = 2; candidate <= num; candidate++) {
+        let isPrime = true
+
+        for(let i = 0; i < primeNumbers.length; i++) {
+            if (candidate % primeNumbers[i] === 0) {
+                isPrime = false
+                break
+            }
+        }
+
+        if (isPrime) {
+            res++
+            indexOfColor = colorsForPrimes[(res-1) % colorsForPrimes.length]
+            console.log(colors[indexOfColor](candidate))
+            primeNumbers.push(candidate)
+        }
+    }
+
+    !res && console.log(colors[colorsForPrimes[0]]('Нет простых чисел'))
+    
+    console.log(' ')
+}
+
+prime(17)
+prime(-1)
+prime('rr')
